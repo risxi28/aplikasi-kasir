a3a8ce8725020e2fa953d17faa59f1347a9a4713
risxi28 <risxibaguseriyana@gmail.com>
2021-12-08 19:46:50 +0700

    first commit

---
+++ src/components/NavbarComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {Nav, Navbar, Container } from 'react-bootstrap'
+
+const NavbarComponent = () => {
+  return (
+    <Navbar bg="success shadow" variant="dark" expand="lg">
+      <Container>
+      <Navbar.Brand href="#home"><strong>Kasir</strong> App</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+  );
+};
+
+export default NavbarComponent;
